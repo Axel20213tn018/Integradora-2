65f23f46979c12e281ec5e94ecd0a6e73b5e31c3
Axel20213tn018 <20213tn018@utez.edu.mx>
2023-02-02 10:52:53 -0600

    Commit: 02/02/2023

---
+++ backend/REDRE/src/main/java/mx/edu/utez/REDRE/RedreApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.REDRE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedreApplication.class, args);
+	}
+
+}
